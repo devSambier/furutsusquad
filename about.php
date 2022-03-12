d8054a7c9fcf69a3c1ae702b40fa5b745f42f8a3
devSambier <100783118+devSambier@users.noreply.github.com>
2022-03-12 20:00:14 +0300

    site

---
+++ about.php
@@ -0,0 +1,444 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Furutsu Squad-Разработчики</title>
+<meta charset="utf-8">
+<meta name="description" content="Участники - Furutsu Squad">
+<meta name="keywords" content="">
+<meta name="author" content="s.wiftzerx">
+<meta name="copyright" content="s.wiftzerx">
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>Участники</h1>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					Немного о скваде и участниках
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<h2>О нас</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+							 Привет, Вас приветствует сквад под названием "Furutsu". Мы создаем сообщество для эдиторов, в котором участники смогут делиться опытом, инструментами которые они используют в своей работе и находить друзей и сообщников) А также это уютная группа, очень ламповых и душевных людей. Играем, веселимся и просто хорошо проводим время!! Присоединяйтесь, ждём каждого!
+						</p>
+					</div>
+				</div>
+			</div>
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/avatars/uzttle.jpg') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/users/flags/eskafla.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							Uzttle </span>
+							<span class="user-con-role user-role-web-sc">
+							✔ Создатель сквада </span>
+							<span class="user-con-role user-role-designer">
+							Участник </span>
+						</div>
+						<div class='user-con-divider role-web darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							Uzttle: Я Аня, или просто узутля, овнер сквада. Редачу в ам, и пробую себя в ае2018 года, свой акк в тт веду по теме аниме, но и некоторые манги тоже проскакивают. Уже год в этой сфере, и стараюсь почучуть совершенствовать свои навыки.</span>
+						</div>
+						<div class="user-con-specsheet">
+							<div class="user-con-specbox">
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/desktop.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>Alight Motion</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/os.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>PC/Android</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/avatars/eskaava.gif') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/users/flags/eskafla.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							s.wiftzerx </span>
+							<span class="user-con-role user-role-web">
+							✔ Создатель сайта </span>
+							<span class="user-con-role user-role-designer">
+							Участник </span>
+						</div>
+						<div class='user-con-divider role-web darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							s.wiftzerx присоединился к Furutsu Squad в 2022 году в качестве веб-разработчика и графического дизайнера сайта. Так же как участник сквада.</span>
+						</div>
+						<div class="user-con-specsheet">
+							<div class="user-con-specbox">
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/desktop.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>After Effects 2018</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/os.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>MacBook 2017</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/avatars/qowka.jpg') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/users/flags/eskafla.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							qokwa </span>
+							<span class="user-con-role user-role-designer">
+							Участник </span>
+						</div>
+						<div class='user-con-divider role-web darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							Работаю в amv. Я Ник. 16 лет. Водолей. При знакомстве довольно холоден и спокоен, однако чуть позже открываюсь людям и становлюсь добродушным милым парнем.</span>
+						</div>
+						<div class="user-con-specsheet">
+							<div class="user-con-specbox">
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/desktop.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>Alight Motion</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/avatars/lilya.jpg') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/users/flags/eskafla.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							libkv.qw </span>
+							<span class="user-con-role user-role-designer">
+							Участник </span>
+						</div>
+						<div class='user-con-divider role-web darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							libkv.qw: Привет, я libkv.qw. Я монтирую в Alight motion/Cap Cut. Мне 14. Люблю кукурузу🌽 Живу в Украине 🇺🇦. Мне больше нечего сказать, ъуы. </span>
+						</div>
+						<div class="user-con-specsheet">
+							<div class="user-con-specbox">
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/desktop.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>Alight Motion/Cap Cut</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/avatars/') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/users/flags/eskafla.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							mngwhub </span>
+							<span class="user-con-role user-role-designer">
+							Участник </span>
+						</div>
+						<div class='user-con-divider role-web darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							mngwhub: Я редактирую в платном видео старе. в основном пользуюсь кодами,но также делаю сама шейки. в видео старе я редактирую уже как два года,и моя история началась с роблокса,и я редактировала видео про него,а потом перешла в тт,и так получилось что теперь я снимаю про аниме,и тд</span>
+						</div>
+						<div class="user-con-specsheet">
+							<div class="user-con-specbox">
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/desktop.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>Video Star</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/avatars/nai.jpg') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/users/flags/eskafla.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							naiihal </span>
+							<span class="user-con-role user-role-designer">
+							Участник </span>
+						</div>
+						<div class='user-con-divider role-web darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							naiihal: Рисую в ibis paint, редактирую видео в кап куте, в основном рисую ары по бсд всем девушкам пива, а мужикам по ебалу 🇺🇦🇺🇦</span>
+						</div>
+						<div class="user-con-specsheet">
+							<div class="user-con-specbox">
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/desktop.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>Cap Cut</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/avatars/kris.jpg') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/users/flags/eskafla.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							mysthl </span>
+							<span class="user-con-role user-role-designer">
+							Участник </span>
+						</div>
+						<div class='user-con-divider role-web darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							mysthl: Меня зовут кристина, редактирую в бесплатном ам, учусь делать что-то в вс и ае. свой аккаунт в основном посвящаю тетради смерти, но так же могут проскакивать и другие аниме и манги/манхвы. его я создала 8 марта этого года и только-только начала развивать, в основном выкладываю самые обычные видео с текстом, эдиты ну очень-очень редко.</span>
+						</div>
+						<div class="user-con-specsheet">
+							<div class="user-con-specbox">
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/desktop.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>Alight Motion Free</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/avatars/') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/users/flags/eskafla.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							_.jir_grwsx._ </span>
+							<span class="user-con-role user-role-designer">
+							Участник </span>
+						</div>
+						<div class='user-con-divider role-web darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							_.jir_grwsx._: Монтирую в алайте. Иногда делаю заготовки в кап кут.Зовут меня Егор, 14 годиков. Люблю музыку/книжки. В свободное время смотрю сериалы или аниме.</span>
+						</div>
+						<div class="user-con-specsheet">
+							<div class="user-con-specbox">
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/desktop.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>Alight Motion</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/avatars/sanya.jpg') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/users/flags/eskafla.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							flama7ter7/Санёк </span>
+							<span class="user-con-role user-role-designer">
+							Участник </span>
+						</div>
+						<div class='user-con-divider role-web darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							flama7ter7: Меня зовут Саша, монтирую я в Alight motion на протяжении года. Я могу делать rotation, raw, flow эдиты, и ещё анимирую мангу. Все это можете посмотреть у меня в тик токе</span>
+						</div>
+						<div class="user-con-specsheet">
+							<div class="user-con-specbox">
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/desktop.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>Alight Motion</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/avatars/med.jpg') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/users/flags/eskafla.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							molle_bliiin </span>
+							<span class="user-con-role user-role-designer">
+							Участник </span>
+						</div>
+						<div class='user-con-divider role-web darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							molle_bliiin: К эдинку пришла случайно.. мне просто нравился сам по себе процесс монтажа:D</span>
+						</div>
+						<div class="user-con-specsheet">
+							<div class="user-con-specbox">
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/desktop.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>Alight Motion</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/avatars/olya.jpg') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/users/flags/eskafla.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							naruxwife </span>
+							<span class="user-con-role user-role-designer">
+							Участник </span>
+						</div>
+						<div class='user-con-divider role-web darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							naruxwife: Я Оля. Мне 14 лет. Люблю монтировать видео и рисовать, а ещё очень обожаю ролтон. Что насчёт моего характера, я тихая и скромная, для близких мне людей - общительная и весёлая.</span>
+						</div>
+						<div class="user-con-specsheet">
+							<div class="user-con-specbox">
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/desktop.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>Alight Motion</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+								<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/avatars/ch.jpg') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/users/flags/eskafla.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							nisekkai </span>
+							<span class="user-con-role user-role-designer">
+							Участник </span>
+						</div>
+						<div class='user-con-divider role-web darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							nisekkai: Привет, я nisekkai.Мне 14 лет, живу в Украине, монтирую в alight motion, однако иногда использую node video. Эдиты делаю по аниме/манге/фильмам. Быстро привязываюсь к человеку, тем самым навязываюсь.</span>
+						</div>
+						<div class="user-con-specsheet">
+							<div class="user-con-specbox">
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/desktop.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>Alight Motion</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+</body>
+</html>
